2445b025e9ffe537988e076129e2acdbe7308e30
Dohyeon Yun <yundohyun050121@gmail.com>
2023-04-26 14:03:30 +0900

    add: `+layout` font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+  <link
+    href="https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/variable/woff2/SUIT-Variable.css"
+    rel="stylesheet"
+  />
+  <style>
+    * {
+      font-family: 'SUIT Variable', sans-serif;
+    }
+  </style>
+</svelte:head>
+<slot />
